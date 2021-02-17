f3c3b1cf7ab67eb998e6bf385c0ce001957bce4a
williagr <williagr@gmail.com>
2021-02-16 20:34:29 -0600

    changes for demo

--- app/src/main/java/data/Inspection_Table.java
+++ app/src/main/java/data/Inspection_Table.java
@@ -4,15 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+import java.util.Date;
 
 @Entity(tableName = "inspection_table")
 public class Inspection_Table {
     @PrimaryKey
-    @NonNull
-    @ColumnInfo(name = "InspectionId")
-    private int mInspectionId;
-
-    @NonNull
-    @ColumnInfo(name = "BuilderId")
-    private int mBuilderId;
+    public int id;
+    public int inspection_number;
+    @TypeConverters({TimestampConverter.class})
+    public Date inspection_date;
+    public int builder_id;
+    public String superintendent;
+    public int location_id;
+    public String inspection_type;
+    public String notes;
+    public boolean is_complete;
+    public boolean is_uploaded;
 }
