9b77c1e041c9627ae4a317eeefb57887b070145f
williagr <williagr@gmail.com>
2022-01-06 18:18:15 -0600

    change to staging

--- app/src/main/java/com/burgess/bridge/BridgeAPIQueue.java
+++ app/src/main/java/com/burgess/bridge/BridgeAPIQueue.java
@@ -89,7 +89,7 @@ public class BridgeAPIQueue {
         BridgeLogger.getInstance(ctx);
 
         // TODO: If true, all endpoints are pointing to BORE, otherwise BOREStage
-        isProd = true;
+        isProd = false;
     }
 
     public static synchronized BridgeAPIQueue getInstance(Context context) {
